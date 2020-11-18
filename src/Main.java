c2c6be66d5bb096297a8e4720040a79badac403d
JK <38530893+JaraKucera@users.noreply.github.com>
2020-11-18 12:17:25 +0000

    Added division method to dataPreprocessing

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
     public static final ArrayList<String> headings = new ArrayList<>(Arrays.asList("calorific_value", "nitrogen", "turbidity", "style", "alcohol", "sugars", "bitterness", "beer_id", "colour", "degree_of_fermentation"));
     // global variable to store the imported Data set
     public static ArrayList<Row> dataSet = new ArrayList<>();
+    //global variable to store dataset to be used for training
+    public static ArrayList<Row> trainingDataset = new ArrayList<>();
+    //global variable to store dataset to be used for testing
+    public static ArrayList<Row> testingDataset = new ArrayList<>();
 
     public static void main(String[] args){
         // Create mock file
@@ -21,6 +25,13 @@ public class Main {
 
         // Print out data
         System.out.println(dataSet);
+
+        //Divide data into thirds, one third is testing, two thirds is training.
+        DataPreProcess.datasetDivision();
+        System.out.println(testingDataset.size());
+        System.out.println(trainingDataset.size());
+        System.out.println(dataSet.size());
+
     }
 
 }
